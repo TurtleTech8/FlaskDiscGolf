40d56cabefe648eaba37899e00c1f591a1492a00
TurtleTech8 <58091353+TurtleTech8@users.noreply.github.com>
2024-02-27 12:31:54 -0600

    Started Shopping Cart Changes
    Added the beginnings of Session variable and the shopping cart page

--- disc_golf_app/views.py
+++ disc_golf_app/views.py
@@ -1,12 +1,18 @@
 from flask import render_template
 from flask import request
+from flask import session
 from . import app
 import disc_golf_app.disc_data as disc_data
 
+app.secret_key = "SUPER_SECRET_KEY"
 
+def checkSession():
+    if 'uid' not in session:
+            session['uid'] = 3
 
 @app.route("/")
 def home():
+    checkSession()
     return render_template(
         "main.html",
         brands=disc_data.getAllBrands(),
@@ -15,10 +21,22 @@ def home():
 
 @app.route("/search")
 def search():
+    checkSession()
+
+    print(session['uid'])
     return render_template(
         "search.html",
         brands=disc_data.getAllBrands(),
         discs=disc_data.getSearchDiscs()
+    )
+
+@app.route("/cart")
+def cart():
+    checkSession()
+
+    print(session['uid'])
+    return render_template(
+        "cart.html"
     )
 
 @app.route("/getDisc")
